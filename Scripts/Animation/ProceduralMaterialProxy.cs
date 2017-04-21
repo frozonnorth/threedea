595fa4af68a66c71d6cbf7ccf934eda42f35a1c3
Igor Rafael de Sousa <igorrafaeldesousa@gmail.com>
2017-04-20 23:59:49 -0400

    WIP: ProceduralMaterialProxy to animate procedural materials

---
+++ Scripts/Animation/ProceduralMaterialProxy.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System;
+
+public class ProceduralMaterialProxy : MonoBehaviour {
+    [Serializable]
+    private abstract class Parameter<T> {
+        public string name;
+        public T value;
+
+        public abstract void Set(ProceduralMaterial pm);
+    }
+    
+    [Serializable]
+    private class Float2 : Parameter<Vector2> {
+        public override void Set(ProceduralMaterial pm) {
+            pm.SetProceduralVector(name, value);
+        }
+    }
+    
+    [Serializable]
+    private class Int : Parameter<int> {
+        public override void Set(ProceduralMaterial pm) {
+            pm.SetProceduralEnum(name, value);
+        }
+    }
+    
+    [SerializeField]
+    Float2[] float2s;
+    [SerializeField]
+    Int[] ints;
+
+
+    void OnValidate() {
+        Renderer renderer = GetComponent<Renderer>();
+        if (!renderer) {
+            return;
+        }
+        ProceduralMaterial material = renderer.material as ProceduralMaterial;
+        if (!material) {
+            return;
+        }
+        
+        for (int i = 0; i < float2s.Length; ++i) {
+            float2s[i].Set(material);
+        }
+        for (int i = 0; i < ints.Length; ++i) {
+            ints[i].Set(material);
+        }
+
+        material.RebuildTexturesImmediately();
+    }
+}
