330032e21642b7a16f9e6ed25b07e44a273ac8ce
Igor Rafael de Sousa <igorrafaeldesousa@gmail.com>
2017-04-24 10:44:28 -0400

    Create tools stubs and Threedea context

---
+++ Scripts/Tools/DrawTool.cs
@@ -0,0 +1,11 @@
+﻿#if UNITY_EDITOR && UNITY_EDITORVR
+
+using UnityEngine;
+using UnityEditor.Experimental.EditorVR;
+
+namespace Threedea.Tools {
+    public class DrawTool : MonoBehaviour, ITool {
+    }
+}
+
+#endif
---
+++ Scripts/Tools/IdeaSelectTool.cs
@@ -0,0 +1,40 @@
+﻿#if UNITY_EDITOR && UNITY_EDITORVR
+
+using UnityEngine;
+using UnityEditor.Experimental.EditorVR;
+using UnityEditor.Experimental.EditorVR.Utilities;
+
+namespace Threedea.Tools {
+    public class IdeaSelectTool : MonoBehaviour, ITool, ISetHighlight {
+        [SerializeField]
+        private LinkHandle linkPrefab;
+        void Start() {
+            var ideas = FindObjectsOfType(typeof(Idea));
+            foreach (Idea idea in ideas) {
+                if (idea.subject && idea.subject != idea) {
+                    LinkHandle link = CreateLink(idea);
+                }
+            }
+        }
+
+        void Update() {
+            var ideas = FindObjectsOfType(typeof(Idea));
+            foreach (Idea obj in ideas) {
+                Debug.DrawLine(Vector3.zero, obj.transform.position);
+            }
+        }
+
+        private LinkHandle CreateLink(Idea idea) {
+            var prefab = linkPrefab.gameObject;
+            var go = ObjectUtils.Instantiate(prefab, runInEditMode: true);
+
+            go.transform.position = idea.transform.position;
+            var link = go.GetComponent<LinkHandle>();
+            link.PositionBetween(idea, idea.subject);
+
+            return link;
+        }
+    }
+}
+
+#endif
